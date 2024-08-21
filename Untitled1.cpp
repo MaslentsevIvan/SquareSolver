2e5ba027a0a6d58b13416e955a48015d8f79e030
MaslentsevIvan <115416840+MaslentsevIvan@users.noreply.github.com>
2024-08-22 00:11:08 +0300

    Update Untitled1.cpp
    Version3

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -6,12 +6,11 @@
 
 void entering_values(double values_coefficients[3]);
 void cleaning_buffer();
-void type_equation(double values_coefficients[3]);
-int def_quantity(double values_coefficients[3]);
+void print_type_equation(double values_coefficients[3]);
+void print_def_quantity(double values_coefficients[3]);
 double calc_disc(double values_coefficients[3]);
 bool compare_doubles(double num1, double num2);
-void calculations(double values_coefficients[3], int amount);
-int def_det(double values_coefficients[3]);
+void calculations(double values_coefficients[3], int det, double solutions[2]);
 
 
 const double EPSILON = 1e-9;
@@ -26,12 +25,12 @@ int main()
     setlocale(LC_ALL, "Russian");
     double values_coefficients[3];
     entering_values(values_coefficients);
-    type_equation(values_coefficients);
-    calculations(values_coefficients, def_quantity(values_coefficients));
-//    for (int i = 0; i < 3; i++)
-//    {
-//        printf("%lf ", values_coefficients[i]);
-//    }
+    print_type_equation(values_coefficients);
+    print_def_quantity(values_coefficients);
+    //    for (int i = 0; i < 3; i++)
+    //    {
+    //        printf("%lf ", values_coefficients[i]);
+    //    }
     return 0;
 }
 
@@ -61,12 +60,12 @@ void entering_values(double values_coefficients[3])
 void cleaning_buffer()
 {
     int ch = 0;
-    while ((ch = getchar()) != '\n' && (ch = getchar()) != EOF) {}
+    while ((ch = getchar()) != '\n') {}
 }
 
 
-//Определение типа уравнения
-void type_equation(double values_coefficients[3])
+//Вывод типа уравнения
+void print_type_equation(double values_coefficients[3])
 {
     if (values_coefficients[0] == 0)
         printf("Это линейное уравнение!\n");
@@ -75,27 +74,30 @@ void type_equation(double values_coefficients[3])
 }
 
 
-//Определение количества решений и значения для переключателя (switch)
-int def_quantity(double values_coefficients[3])
+//Вывод решения
+void print_def_quantity(double values_coefficients[3])
 {
     double a, b, c;
+    double solutions[2];
     a = values_coefficients[0];
     b = values_coefficients[1];
     c = values_coefficients[2];
     double disc = calc_disc(values_coefficients);
     if (a == 0 && b == 0 && c == 0)
-        printf("Количество решений - INF.\n");
+        printf("Количество решений - INF.");
     if ((a == 0 && b == 0 && c != 0) || (disc < 0 && a != 0))
-        printf("Количество решений - 0.\n");
-    if ((a == 0 && b != 0) || (compare_doubles(disc, 0) == true && a != 0))
+        printf("Количество решений - 0.");
+    if ((a == 0 && b != 0) || (compare_doubles(disc, 0) && a != 0))
     {
         printf("Количество решений - 1.\n");
-        return 1;
+        calculations(values_coefficients, one_root, solutions);
+        printf("x = %lf", solutions[0]);
     }
     if (disc > 0 && a != 0)
     {
         printf("Количество решений - 2.\n");
-        return 2;
+        calculations(values_coefficients, two_roots, solutions);
+        printf("x1 = %lf\nx2 = %lf", solutions[0], solutions[1]);
     }
 }
 
@@ -118,24 +120,21 @@ bool compare_doubles(double num1, double num2)
 }
 
 
-//Вычисление решений и их вывод
-void calculations(double values_coefficients[3], int det)
+//Вычисление корней уравнения
+void calculations(double values_coefficients[3], int det, double solutions[2])
 {
-    double x1, x2;
     switch (det)
     {
     case one_root:
         if (values_coefficients[0] == 0)
-            x1 = ((-1) * values_coefficients[2]) / values_coefficients[1];
+            solutions[0] = ((-1) * values_coefficients[2]) / values_coefficients[1];
         else
-            x1 = ((-1) * values_coefficients[1]) / (2 * values_coefficients[0]);
-        printf("x = %lf", x1);
+            solutions[0] = ((-1) * values_coefficients[1]) / (2 * values_coefficients[0]);
         break;
     case two_roots:
         double disc = calc_disc(values_coefficients);
-        x1 = ((-1) * values_coefficients[1] - sqrt(disc)) / (2 * values_coefficients[0]);
-        x2 = ((-1) * values_coefficients[1] + sqrt(disc)) / (2 * values_coefficients[0]);
-        printf("x1 = %lf\nx2 = %lf", x1, x2);
+        solutions[0] = ((-1) * values_coefficients[1] - sqrt(disc)) / (2 * values_coefficients[0]);
+        solutions[1] = ((-1) * values_coefficients[1] + sqrt(disc)) / (2 * values_coefficients[0]);
         break;
     }
 }
