c46908521e39a45c6affe1eedf2ca5b5b2101ef2
MaslentsevIvan <115416840+MaslentsevIvan@users.noreply.github.com>
2024-08-23 19:23:41 +0300

    Update Untitled1.cpp
    Version4

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,51 +1,96 @@
 //#include <TXLib.h>
 #include <stdio.h>
 #include <iostream>
-#include <cmath>
+#include <math.h>
+#include <assert.h>
 
 
-void entering_values(double values_coefficients[3]);
+struct params_eq
+{
+    double a = NAN;
+    double b = NAN;
+    double c = NAN;
+};
+
+
+struct ans_eq
+{
+    bool is_linear = false;
+    int roots_numb = -1;
+    double x1 = NAN;
+    double x2 = NAN;
+};
+
+
+void solver_quad_equation();
+void entering_values(struct params_eq* const coefficients);
 void cleaning_buffer();
-void print_type_equation(double values_coefficients[3]);
-void print_def_quantity(double values_coefficients[3]);
-double calc_disc(double values_coefficients[3]);
-bool compare_doubles(double num1, double num2);
-void calculations(double values_coefficients[3], int det, double solutions[2]);
+int def_quantity(const struct params_eq coefficients, const struct ans_eq* const solutions, const double disc);
+double calc_disc(const struct params_eq coefficients);
+bool compare_doubles(const double num1, const double num2);
+void calculations(const struct params_eq coefficients, struct ans_eq* const solutions, const double disc);
+void print_solutions(const struct params_eq coefficients, struct ans_eq solutions);
+void is_eq_linear(const struct params_eq coefficients, struct ans_eq* const solutions);
+void sort_roots(struct ans_eq* const solutions);
+double zero_mistake(const double x);
+void next_eq(int* const selector);
 
 
 const double EPSILON = 1e-9;
-const char name_coefficients[] = "abc";
-const int one_root = 1;
-const int two_roots = 2;
+const char NAME_COEFFICIENTS[] = "abc";
+enum roots
+{
+    NO_ROOT = 0,
+    ONE_ROOT = 1,
+    TWO_ROOTS = 2,
+    INF_ROOTS = 3,
+};
 
 
-//Основная программа
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    double values_coefficients[3];
-    entering_values(values_coefficients);
-    print_type_equation(values_coefficients);
-    print_def_quantity(values_coefficients);
-    //    for (int i = 0; i < 3; i++)
-    //    {
-    //        printf("%lf ", values_coefficients[i]);
-    //    }
+    int selector = -1;
+    do
+    {
+        solver_quad_equation(); /*Главная функция по решению квадратных уравнений*/
+        next_eq(&selector); /*Функция перехода к следующему уравнению*/
+    } while (selector);
+    printf("Конец программы!");
     return 0;
 }
 
 
-//Ввод коэффициентов
-void entering_values(double values_coefficients[3])
+//Главная функция по решению квадратных уравнений
+void solver_quad_equation()
 {
+    struct params_eq coefficients;
+    struct ans_eq solutions;
+    entering_values(&coefficients); /*Ввод коэффициентов*/
+    is_eq_linear(coefficients, &solutions); /*Считать ли дискриминант???*/
+    double disc = NAN;
+    if (!(solutions.is_linear))
+        disc = calc_disc(coefficients); /*Вычисление дискриминанта*/
+    solutions.roots_numb = def_quantity(coefficients, &solutions, disc); /*Определение количества решений*/
+    calculations(coefficients, &solutions, disc); /*Вычисление корней уравнения*/
+    print_solutions(coefficients, solutions); /*Вывод ответа*/
+}
+
+
+//Ввод коэффициентов
+void entering_values(struct params_eq* const coefficients)
+{
+    assert(coefficients != NULL);
+
+    double* values_coefficients[3] = { &((*coefficients).a), &((*coefficients).b), &((*coefficients).c) };
     for (int i = 0; i < 3; i++)
     {
         char tmp = 0;
         int res = 0;
         do
         {
-            printf("Введите коэффициент %c: ", name_coefficients[i]);
-            res = scanf("%lf%c", &values_coefficients[i], &tmp);
+            printf("Введите коэффициент %c: ", NAME_COEFFICIENTS[i]);
+            res = scanf_s("%lf%c", values_coefficients[i], &tmp);
             if (tmp != '\n' || res != 2)
             {
                 printf("Ошибка!\nМожно вводить только числа.\n");
@@ -60,81 +105,161 @@ void entering_values(double values_coefficients[3])
 void cleaning_buffer()
 {
     int ch = 0;
-    while ((ch = getchar()) != '\n') {}
+    while ((ch = getchar()) != '\n' && ch != EOF) {}
 }
 
 
-//Вывод типа уравнения
-void print_type_equation(double values_coefficients[3])
+//Определение количества решений
+int def_quantity(const struct params_eq coefficients, const struct ans_eq* const solutions, const double disc)
 {
-    if (values_coefficients[0] == 0)
-        printf("Это линейное уравнение!\n");
-    else
-        printf("Это квадратное уравнение!\n");
-}
+    assert(solutions != NULL);
 
-
-//Вывод решения
-void print_def_quantity(double values_coefficients[3])
-{
-    double a, b, c;
-    double solutions[2];
-    a = values_coefficients[0];
-    b = values_coefficients[1];
-    c = values_coefficients[2];
-    double disc = calc_disc(values_coefficients);
-    if (a == 0 && b == 0 && c == 0)
-        printf("Количество решений - INF.");
-    if ((a == 0 && b == 0 && c != 0) || (disc < 0 && a != 0))
-        printf("Количество решений - 0.");
-    if ((a == 0 && b != 0) || (compare_doubles(disc, 0) && a != 0))
+    if ((*solutions).is_linear)
     {
-        printf("Количество решений - 1.\n");
-        calculations(values_coefficients, one_root, solutions);
-        printf("x = %lf", solutions[0]);
+        if (compare_doubles(coefficients.b, 0) && compare_doubles(coefficients.c, 0))
+            return INF_ROOTS;
+        else if (compare_doubles(coefficients.b, 0) && !(compare_doubles(coefficients.c, 0)))
+            return NO_ROOT;
+        else
+            return ONE_ROOT;
     }
-    if (disc > 0 && a != 0)
+    else
     {
-        printf("Количество решений - 2.\n");
-        calculations(values_coefficients, two_roots, solutions);
-        printf("x1 = %lf\nx2 = %lf", solutions[0], solutions[1]);
+        assert(isfinite(disc));
+
+        if (disc > 0)
+            return TWO_ROOTS;
+        if (disc < 0)
+            return NO_ROOT;
+        else
+            return ONE_ROOT;
     }
 }
 
 
 //Вычисление дискриминанта
-double calc_disc(double values_coefficients[3])
+double calc_disc(const struct params_eq coefficients)
 {
-    double disc = values_coefficients[1] * values_coefficients[1] - 4 * values_coefficients[0] * values_coefficients[2];
-    return disc;
+    return coefficients.b * coefficients.b - 4 * coefficients.a * coefficients.c;
 }
 
 
 //Сравнение чисел с плавающей точкой (Если числа равны, то возвращается 1)
-bool compare_doubles(double num1, double num2)
+bool compare_doubles(const double num1, const double num2)
 {
-    if (fabs(num1 - num2) < EPSILON)
-        return true;
-    else
-        return false;
+    assert(isfinite(num1) && isfinite(num2));
+
+    return (fabs(num1 - num2) < EPSILON);
 }
 
 
 //Вычисление корней уравнения
-void calculations(double values_coefficients[3], int det, double solutions[2])
+void calculations(const struct params_eq coefficients, struct ans_eq* const solutions, const double disc)
 {
-    switch (det)
+    assert(solutions != NULL);
+
+    switch ((*solutions).roots_numb)
     {
-    case one_root:
-        if (values_coefficients[0] == 0)
-            solutions[0] = ((-1) * values_coefficients[2]) / values_coefficients[1];
+    case ONE_ROOT:
+        if ((*solutions).is_linear)
+            (*solutions).x1 = ((-1) * coefficients.c) / coefficients.b;
         else
-            solutions[0] = ((-1) * values_coefficients[1]) / (2 * values_coefficients[0]);
+            (*solutions).x1 = ((-1) * coefficients.b) / (2 * coefficients.a);
         break;
-    case two_roots:
-        double disc = calc_disc(values_coefficients);
-        solutions[0] = ((-1) * values_coefficients[1] - sqrt(disc)) / (2 * values_coefficients[0]);
-        solutions[1] = ((-1) * values_coefficients[1] + sqrt(disc)) / (2 * values_coefficients[0]);
+    case TWO_ROOTS:
+        assert(isfinite(disc));
+
+        const double sqrt_disc = sqrt(disc);
+        (*solutions).x1 = ((-1) * coefficients.b - sqrt_disc) / (2 * coefficients.a);
+        (*solutions).x2 = ((-1) * coefficients.b + sqrt_disc) / (2 * coefficients.a);
         break;
     }
+}
+
+
+//Вывод ответа
+void print_solutions(const struct params_eq coefficients, struct ans_eq solutions)
+{
+    if (solutions.is_linear)
+        printf("Это линейное уравнение!\n");
+    else
+        printf("Это квадратное уравнение!\n");
+    switch (solutions.roots_numb)
+    {
+    case NO_ROOT:
+        printf("Количество решений - 0.\n");
+        break;
+    case INF_ROOTS:
+        printf("Количество решений - INF.\n");
+        break;
+    case ONE_ROOT:
+        solutions.x1 = zero_mistake(solutions.x1);
+        printf("Количество решений - 1.\n");
+        printf("x = %lf\n", solutions.x1);
+        break;
+    case TWO_ROOTS:
+        solutions.x1 = zero_mistake(solutions.x1);
+        solutions.x2 = zero_mistake(solutions.x2);
+        sort_roots(&solutions);
+        printf("Количество решений - 2.\n");
+        printf("x1 = %lf\nx2 = %lf\n", solutions.x1, solutions.x2);
+        break;
+    }
+}
+
+
+//Считать ли дискриминант???
+void is_eq_linear(const struct params_eq coefficients, struct ans_eq* const solutions)
+{
+    assert(solutions != NULL);
+
+    if (compare_doubles(coefficients.a, 0))
+        (*solutions).is_linear = 1;
+    else
+        (*solutions).is_linear = 0;
+}
+
+
+//Функция для вывода от меньшего к большего
+void sort_roots(struct ans_eq* const solutions)
+{
+    assert(solutions != NULL);
+
+    if ((*solutions).x1 > (*solutions).x2)
+    {
+        double tmp = (*solutions).x1;
+        (*solutions).x1 = (*solutions).x2;
+        (*solutions).x2 = tmp;
+    }
+}
+
+
+//Проверка на ошибку с нулём
+double zero_mistake(const double x)
+{
+    assert(isfinite(x));
+
+    if (compare_doubles(x, 0))
+        return fabs(x);
+    else
+        return x;
+}
+
+
+//Функция перехода к следующему уравнению
+void next_eq(int* const selector)
+{
+    char tmp = 0;
+    const int flag = 1;
+    int res = 0;
+    do
+    {
+        printf("\nХотите ли вы ввести новое уравнене? (1 - Да, 0- Нет) - ");
+        res = scanf_s("%d%c", selector, &tmp);
+        if (!(tmp == '\n' && ((*selector) == 0 || (*selector) == 1)))
+            printf("Ошибка!\nМожно вводить 0 или 1.\n");
+        if (res != 2)
+            cleaning_buffer();
+    } while (tmp != '\n' || res != 2 || (*selector != 1 && *selector != 0));
+    printf("\n");
 }
